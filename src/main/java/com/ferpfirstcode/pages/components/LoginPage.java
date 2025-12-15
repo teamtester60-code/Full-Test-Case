8423e5b2dde1475a3f645083b142eb0106c9724c
AhmedHassan1 <teamtester60@gmail.com>
2025-12-15 14:02:31 +0200

    test11

--- src/main/java/com/ferpfirstcode/pages/components/LoginPage.java
+++ src/main/java/com/ferpfirstcode/pages/components/LoginPage.java
@@ -179,6 +179,7 @@ public class LoginPage {
         driver.element().clickElement(pinicon);
         ScreenShotsManager.takeFullPageScreenshot(driver.get(),"AfterPin");
         driver.element().typeText(passwordField, pin);
+        ScreenShotsManager.takeFullPageScreenshot(driver.get(),"Aftertyping");
         return this;
     }
 
